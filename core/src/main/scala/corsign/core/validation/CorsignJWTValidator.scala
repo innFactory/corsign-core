d78e6b813d44acff5f88c4ac66524130b2cdcecc
Tobias Jonas <t.jonas@innFactory.de>
2021-04-08 12:50:00 +0200

    update jwk max size

--- core/src/main/scala/corsign/core/validation/CorsignJWTValidator.scala
+++ core/src/main/scala/corsign/core/validation/CorsignJWTValidator.scala
@@ -1,15 +1,18 @@
 package corsign.core.validation
 
 import java.net.URL
-import com.nimbusds.jose.jwk.source.{JWKSource, RemoteJWKSet}
+import com.nimbusds.jose.jwk.source.{DefaultJWKSetCache, JWKSource, RemoteJWKSet}
 import com.nimbusds.jose.proc.SecurityContext
+import com.nimbusds.jose.util.DefaultResourceRetriever
 import com.nimbusds.jwt.JWTClaimsSet
 import com.nimbusds.jwt.proc.BadJWTException
 import corsign.core.jwk.JWKUrl
 import corsign.core.jwt.{JWTAlgorithm, JWTClaims}
 import corsign.core.jwt.ProvidedValidations._
+import corsign.core.validation.CorsignJWTValidator.DEFAULT_HTTP_SIZE_LIMIT
 
 object CorsignJWTValidator {
+  val DEFAULT_HTTP_SIZE_LIMIT = 25 * 1024 * 1024
   def apply(
     url: JWKUrl
   ): CorsignJWTValidator = new CorsignJWTValidator(url)
@@ -19,7 +22,7 @@ final class CorsignJWTValidator(url: JWKUrl) extends JWTValidator {
 
   private val issuer = url.value
 
-  private val jwkSet: JWKSource[SecurityContext] = new RemoteJWKSet(new URL(s"${url.value}/.well-known/jwks.json"))
+  private val jwkSet: JWKSource[SecurityContext] = new RemoteJWKSet(new URL(s"${url.value}/.well-known/jwks.json"), new DefaultResourceRetriever(4000, 4000, DEFAULT_HTTP_SIZE_LIMIT))
 
   private val configurableJwtValidator =
     new ConfigurableJWTValidator(
