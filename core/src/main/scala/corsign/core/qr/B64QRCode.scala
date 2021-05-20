39ef981bd28ccb5e25b59e3db1b54cde5084c5db
Tobias Jonas <t.jonas@innfactory.de>
2021-05-20 14:17:40 +0200

    1.15

--- core/src/main/scala/corsign/core/qr/B64QRCode.scala
+++ core/src/main/scala/corsign/core/qr/B64QRCode.scala
@@ -1,5 +1,7 @@
 package corsign.core.qr
 
+import com.google.zxing.EncodeHintType
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
 import net.glxn.qrgen.core.image.ImageType
 import net.glxn.qrgen.javase.QRCode
 import org.apache.commons.codec.binary.Base64
@@ -14,6 +16,7 @@ case class B64QRCode() extends GenericQRCode[String] {
         .withColor(qrInfo.onColor, qrInfo.offColor)
         .withSize(qrInfo.width, qrInfo.height)
         .withCharset("UTF-8")
+        .withHint(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.Q)
         .stream()
         .toByteArray
     )
