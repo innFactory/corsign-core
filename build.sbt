08e011654e1dd9f487bca961bcb5f9ba5a2c721b
patsta32 <stadler_patrick@t-online.de>
2021-05-20 14:22:12 +0200

    add additional testinformation to CorData

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 
 val appName    = "corsign-core"
-val libVersion = "1.1.5"
+val libVersion = "1.1.6"
 val scVersion  = "2.13.5"
 
 name := appName
--- core/src/main/scala/corsign/core/model/CorData.scala
+++ core/src/main/scala/corsign/core/model/CorData.scala
@@ -4,13 +4,18 @@ import play.api.libs.json.{JsValue, Json}
 
 case class CorData(
                     isNegative: Option[Boolean] = None,
-                    testType: Option[String] = None,
+                    testType: Option[String] = None, //pcr-test  pcr-rapid-test antigen-rapid-test ag-rapid-test-w-supervision
                     testId: Option[String] = None,
+                    invalid: Option[Boolean] = None,
                     isVaccinated: Option[Boolean] = None,
                     isImmune: Option[Boolean] = None,
                     vaccine: Option[String] = None,
                     appData1: Option[JsValue] = None,
-                    appData2: Option[JsValue] = None
+                    appData2: Option[JsValue] = None,
+                    creatorType: Option[String] = None,
+                    carriedOutBy: Option[String] = None,
+                    testManufacturer: Option[String] = None,
+                    testName: Option[String] = None,
 )
 
 object CorData {
